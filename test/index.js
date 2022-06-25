7ce77e251ba8b973bdfe14288751cdc8fbebf793
Vũ Hoàng Đức <duc10a2lc97@gmail.com>
2022-06-25 19:35:10 +0700

    summary

---
+++ test/index.js
@@ -0,0 +1,38 @@
+let b = document.getElementById('test');
+let board = [];
+let data = "";
+
+for(let i = 0; i < 6; i++){
+    board[i] = new Array(".", ".", ".", ".", ".", ".");
+}
+console.log(board, "board");
+for(let i = 0; i < 6; i++){
+    data += "</br></br>"
+
+for(let j = 0; j < 6; j++){
+    data += board[i][j]+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp";
+    }
+}
+
+b.innerHTML = data;
+
+let check_x = true;
+function changeValue() {
+    let positionX = + prompt("X: ");
+    let positionY = + prompt("Y: ");
+    data = "";
+    if (check_x) {
+        board[positionX][positionY] = "x";
+    }
+    else {    
+        board[positionX][positionY] = "o";
+    }
+
+    for(let i = 0; i < 6; i++){;
+    data += '<br/><br/>';}
+    for(let j = 0; j < 6; j++) {;
+    data = board[i][j]+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp";
+    }
+}
+
+b.innerHTML = data;
